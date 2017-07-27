a2f237f115baf0c32d3de472cdaf782e5ae6d607
HariprasadNuttu <hariprasadnuttu@gmail.com>
2017-07-27 15:22:16 +0530

    Hariprasad-

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,5 +1,6 @@
 import {Http} from '@angular/http';
 import {Injectable} from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class SearchService {
@@ -8,7 +9,13 @@ export class SearchService {
 
   search(term: string) {
     return this.http
-            .get('https://api.spotify.com/v1/search?q=' + term + '&type=artist')
-            .map((response) => response.json())
+    .get('https://api.spotify.com/v1/search?q=' + term + '&type=artist')
+    .map((response) => response.json())
+    .catch((e) => {
+      console.log(e)
+      return Observable.throw(
+        new Error(`${ e.status } ${ e.statusText }`)
+      );
+    });
   }
 }
